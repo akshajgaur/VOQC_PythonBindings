e47df1f44c9fcccf1127732945b5ac9a8cf80646
akshajgaur <akshajgaur@gmail.com>
2020-07-10 04:26:43 -0700

    Fixed write_qasm_file

--- SQIR/VOQC/extraction/_build/generated/main.c
+++ SQIR/VOQC/extraction/_build/generated/main.c
@@ -4,7 +4,6 @@
 int main(){
   struct tuples xg = {{1, 0},1};
   struct triples cnotg = {{3, 0}, 2, 8};
-  struct triples cnotg = {{4, 1.9}, 2, 8};
   struct gate_app1 first;
   first.App1 = xg;
   first.ans = 1;
@@ -23,8 +22,24 @@ int main(){
   printf("%d\n", pass.contents[1].App2.a);
   printf("%d\n", pass.contents[1].App2.b);
   printf("%s\n", "---------------------------------");
-
-  
+  struct with_qubits *gate_l;
+  gate_l = get_gate_list("tof_3.qasm");
+  int i=0;
+  for (i = 0; i<(gate_l->length);i++){
+  	struct gate_app1 temp;
+  	temp = gate_l ->contents[i];
+  	if (temp.App1.gate.gates ==0 && temp.App2.gate1.gates==0){
+  	temp.ans = 3;
+  	gate_l->contents[i] = temp;
+  	}else if(temp.App2.gate1.gates ==0 && temp.App3.gate2.gates==0){
+  	temp.ans = 1;
+  	gate_l->contents[i]=temp;
+  	}else{
+  	temp.ans = 2;
+  	gate_l->contents[i] = temp;
+  	}
+  }
+  printf("%d\n\n\n", gate_l->qubits);
   struct internal *get;
   get =not_propagation(&pass);
   struct gate_app1 f;
@@ -38,6 +53,7 @@ int main(){
   printf("%s\n", "Output 2");
   printf("%d\n", s.App1.gate.gates);
   printf("%d\n", s.App1.x);
+  write_qasm_file("me", gate_l);
   
 
   
