10a18a3e6cf0e1ecfa750d801729dab40975b002
akshajgaur <akshajgaur@gmail.com>
2020-07-13 06:04:26 -0700

    Fixed qubits

--- SQIR/VOQC/extraction/_build/generated/voqc.py
+++ SQIR/VOQC/extraction/_build/generated/voqc.py
@@ -26,12 +26,14 @@ class quad(Structure):
 class gate_app1(Structure):
     _fields_ = [('App1', tuples), ('App2', triples), ('App3', quad),('ans', c_int)]
     
-GATE_APP = gate_app1 *100
+GATE_APP = gate_app1 *1000
 
 class with_qubits(Structure):
     _fields_ = [('length', c_int), ('contents2', GATE_APP), ('qubits', c_int)]
 def convert_gates(x):
     y = 0
+    num = 0
+    den = 0
     if x == 'X':
         y = 1
     elif x == 'H':
@@ -40,17 +42,21 @@ def convert_gates(x):
         y =3
     else:
         y=4
-    return y
-def format_to_c(py_list):
+        num = int(x[3])
+        den = int(x[5])
+    return y, num, den
+def format_to_c(py_list,q):
     struct_return = GATE_APP()
     struct_app = gate_app1()
     tot_length = len(py_list)
     for i in range(tot_length):
         sub_len = len(py_list[i])
-        int_val = convert_gates(py_list[i][0])
-        zarith = py_list[i][0].split()
-        temp = final_gates(gates = int_val)
-            
+        conv = (convert_gates(py_list[i][0]))
+        int_val = conv[0]
+        numer = conv[1]
+        denom = conv[2]
+        rat = rational(numer, denom)
+        temp = final_gates(int_val, rat)
         if sub_len == 2:
             tup = tuples(temp, int(py_list[i][1]))
             struct_app=gate_app1(App1=tup, ans =1)
@@ -61,7 +67,7 @@ def format_to_c(py_list):
              tup = quad(temp, int(py_list[i][1]),int(py_list[i][2]),int(py_list[i][3]))
              struct_app=gate_app1(App3=tup, ans =3)
         struct_return[i] = struct_app
-    with_q = with_qubits(tot_length, struct_return, 0)
+    with_q = with_qubits(tot_length, struct_return,int(q))
     return with_q
 def get_text(x,y,z):
     gate_get = {1:'X', 2: 'H', 3:'CNOT', 4: 'Rz'+ ' ' +str(y) + '/' + str(z)}
@@ -100,10 +106,11 @@ def get_gate_list(fname, out):
     circ = testlib.get_gate_list(final_file)
     q = circ.contents.qubits
     temp = format_from_c(circ)
-    f = open(out, "w+")
-    with open(out, 'w') as f:
+    with open(out+'.txt', 'w') as f:
         for item in temp:
             f.write("%s\n" % item)
+        f.write("%s\n" % q)
+        
     print(format_from_c(circ), q)
 
 
@@ -117,6 +124,7 @@ def voqc(fname, out):
     
 def optimize(fname, out):
     to_py = []
+    get_q = []
     input1 = open(fname +'.txt')
     for i in (input1.readlines()):
         to_py.append(i)
@@ -126,19 +134,21 @@ def optimize(fname, out):
     for i in range(len(new_py)):
         if new_py[i]!= ' ':
             fin_py.append(ast.literal_eval(new_py[i]))
+    q = int(fin_py.pop())
     testlib = CDLL('./libvoqc.so')
     testlib.optimizer.argtypes =[POINTER(with_qubits)]
     testlib.optimizer.restype =POINTER(with_qubits)
-    with_c = format_to_c(fin_py)
+    with_c = format_to_c(fin_py, q)
     temp = format_from_c(testlib.optimizer(byref(with_c)))
-    with open(out, 'w') as f:
+    with open(out+ '.txt', 'w') as f:
         for item in temp:
             f.write("%s\n" % item)
+        f.write("%d\n" % q)
     print(temp)
     
-def write_qasm_file(out, sqir):
+def write_qasm_file(out, fname):
     to_py = []
-    input1 = open(fname +'.txt')
+    input1 = open(out +'.txt')
     for i in (input1.readlines()):
         to_py.append(i)
     new_py = []
@@ -147,15 +157,13 @@ def write_qasm_file(out, sqir):
     for i in range(len(new_py)):
         if new_py[i]!= ' ':
             fin_py.append(ast.literal_eval(new_py[i]))
+    q = int(fin_py.pop())
     testlib = CDLL('./libvoqc.so')
-    testlib.not_propagation.argtypes =[POINTER(with_qubits)]
-    testlib.not_propagation.restype =POINTER(with_qubits)
-    with_c = format_to_c(fin_py)
-    temp = format_from_c(testlib.not_propagation(byref(with_c)))
-    with open(out, 'w') as f:
-        for item in temp:
-            f.write("%s\n" % item)
-    print(temp)
+    testlib.write_qasm_file.argtypes =[c_char_p, POINTER(with_qubits)]
+    testlib.write_qasm_file.restype =None
+    with_c = format_to_c(fin_py,q)
+    out_file = str(fname).encode('utf-8')
+    testlib.write_qasm_file(out_file,byref(with_c))
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Pass a function name and then its parameters")
@@ -169,6 +177,8 @@ if __name__ == '__main__':
         get_gate_list(args.i, args.o)
     elif args.function=='optimize':
         optimize(args.i, args.o)
+    elif args.function == 'write_qasm_file':
+        write_qasm_file(args.i, args.o)
 
         
 
